401001f31bf4d41d7380a38abb525ccf23420737
Thanh <thanh1705aurai@gmail.com>
2025-05-09 19:04:35 +0700

    fix bug bug bị lỗi

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,11 @@ use Inertia\Inertia;
 //     return Inertia::render('welcome');
 // })->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
-//     Route::get('dashboard', function () {
-//         return Inertia::render('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
