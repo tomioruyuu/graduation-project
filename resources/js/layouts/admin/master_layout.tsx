860918349c7fd932b57b9bd48fb791fde866a114
Thanh <thanh1705aurai@gmail.com>
2025-05-09 17:22:05 +0700

    bổ sung footer

--- resources/js/layouts/admin/master_layout.tsx
+++ resources/js/layouts/admin/master_layout.tsx
@@ -1,15 +1,16 @@
-import Header from './partials/header'
-import Sidebar from './partials/sidebar'
+import Footer from './partials/footer';
+import Header from './partials/header';
+import Sidebar from './partials/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen bg-gray-50 text-gray-800">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="p-6 flex-1 overflow-y-auto">{children}</main>
-      </div>
-    </div>
-  )
+    return (
+        <div className="flex min-h-screen bg-gray-50 text-gray-800">
+            <Sidebar />
+            <div className="flex flex-1 flex-col">
+                <Header />
+                <main className="flex-1 overflow-y-auto p-6">{children}</main>
+                <Footer />
+            </div>
+        </div>
+    );
 }
-    
