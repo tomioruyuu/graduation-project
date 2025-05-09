0e3364640d97ff3bcfb939c62a7846dc0d27c044
Thanh <thanh1705aurai@gmail.com>
2025-05-09 16:16:12 +0700

    kiểm tra lại giao diện

---
+++ resources/js/layouts/admin/partials/footer.tsx
@@ -0,0 +1,13 @@
+// resources/js/layouts/admin/partials/footer.tsx
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="pull-right">
+        gentelella - bootstrap admin template by <a href="https://colorlib.com">colorlib</a>
+      </div>
+      <div className="clearfix" />
+    </footer>
+  )
+}
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('admin/dashboard');
+    })->name('admin.dashboard');
+});
